a5ce88f14319946d4e80c79c43edd0f7fc302354
nay man <nayman.po@gmail.com>
2018-09-03 08:05:15 +0700

    add persitence repository class

---
+++ Query/Persistence/Repository.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+using resposity_enity_csharp.Query.Core.Repositories;
+
+namespace resposity_enity_csharp.Query.Persistence
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        public Repository( DbContext context ){
+     
+            Context = context;
+        }
+        public void Add( TEntity entity )
+        {
+            
+            Context.Set< TEntity >().Add(entity);
+        }
+
+        public void AddRange( IEnumerable<TEntity> entities )
+        {
+            Context.Set< TEntity >().AddRange( entities );
+        }
+
+        public IEnumerable<TEntity> Find( Expression<System.Func<TEntity, bool>> predicate )
+        {
+            return Context.Set<TEntity>().Where( predicate );
+        }
+
+        public TEntity Get( int id )
+        {
+            return Context.Set<TEntity>().Find( id );
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Context.Set< TEntity >().ToList();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set< TEntity >().Remove( entity );
+        }
+
+        public void RemoveRange( IEnumerable<TEntity> entities )
+        {
+            Context.Set< TEntity >().RemoveRange( entities );
+        }
+    }
+}
